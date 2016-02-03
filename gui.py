2f2181963af1aa1e03e3084d2fe48c270189a615
Cyrille Rossant <cyrille.rossant@gmail.com>
2016-02-03 14:01:34 +0100

    Update

--- gui.py
+++ gui.py
@@ -2,6 +2,8 @@
 
 import logging
 
+import numpy as np
+
 from phy.gui import create_app, run_app
 from phycontrib.template import create_template_gui
 
@@ -13,6 +15,7 @@ kwargs = dict(
     n_channels_dat=129,
     sample_rate=25000.,
     n_samples_waveforms=30,
+    dtype=np.int16,
 )
 
 create_app()
