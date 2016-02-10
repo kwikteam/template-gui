b4af2b562f55eab78ab17c9f681b72e8d1ac4cb0
Cyrille Rossant <cyrille.rossant@gmail.com>
2016-02-10 17:39:37 +0100

    Add debug logger

--- gui.py
+++ gui.py
@@ -4,12 +4,14 @@ import logging
 
 import numpy as np
 
+from phy import add_default_handler
 from phy.utils._misc import _read_python
 from phy.gui import create_app, run_app
 from phycontrib.template import create_template_gui, TemplateController
 
-
 logger = logging.getLogger(__name__)
+
+add_default_handler('DEBUG')
 
 params = _read_python('params.py')
 params['dtype'] = getattr(np, params['dtype'], 'int16')
