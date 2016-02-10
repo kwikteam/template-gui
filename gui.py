212f2eb1fa0476b22e7fc670d90bc463d079ee80
Cyrille Rossant <cyrille.rossant@gmail.com>
2016-02-10 19:56:13 +0100

    Update

--- gui.py
+++ gui.py
@@ -7,7 +7,7 @@ import numpy as np
 from phy import add_default_handler
 from phy.utils._misc import _read_python
 from phy.gui import create_app, run_app
-from phycontrib.template import create_template_gui, TemplateController
+from phycontrib.template import create_template_gui
 
 logger = logging.getLogger(__name__)
 
@@ -15,11 +15,6 @@ add_default_handler('DEBUG')
 
 params = _read_python('params.py')
 params['dtype'] = getattr(np, params['dtype'], 'int16')
-
-# tc = TemplateController(**params)
-# d = tc.get_features([10])
-# print(d)
-# exit()
 
 create_app()
 plugins = ['SaveGeometryStatePlugin',
